85f85c1e7011d8f1ceb4008f790051feeaeedd27
paula <paulapscx@gmail.com>
2023-04-19 18:04:46 +0200

    remove items add items

--- src/Money.java
+++ src/Money.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
 
+
 public class Money {
 
   private LocalDate date;
@@ -76,6 +77,21 @@ public class Money {
 
   public void setSum(int sum) {
     this.sum = sum;
+  }
+  public class AddRemoveItem {
+    private List<String> items;
+
+    public AddRemoveItem() {
+      items = new ArrayList<>();
+    }
+
+    public void addItem(String item) {
+      items.add(item);
+    }
+
+    public void removeItem(String item) {
+      items.remove(item);
+    }
   }
 }
 
