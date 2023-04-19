8f11e23d6bb2b1d74ce25d9622839a896ab879d4
paula <paulapscx@gmail.com>
2023-04-19 18:47:17 +0200

    added month list

--- src/BudgetPeriod.java
+++ src/BudgetPeriod.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 public class BudgetPeriod {
+        public static void main(String[] args) {
+            List<String> monthList = new ArrayList<>();
+            monthList.add("Январь");
+            monthList.add("Февраль");
+            monthList.add("Март");
+            monthList.add("Апрель");
+            monthList.add("Май");
+            monthList.add("Июнь");
+            monthList.add("Июль");
+            monthList.add("Август");
+            monthList.add("Сентябрь");
+            monthList.add("Октябрь");
+            monthList.add("Ноябрь");
+            monthList.add("Декабрь");
 
+            System.out.println("Список месяцев: " + monthList);
+        }
 }
