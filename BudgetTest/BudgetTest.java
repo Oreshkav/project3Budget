cc23c6fb706551e1e7c48a372dea67bd9bf06a2a
paula <paulapscx@gmail.com>
2023-04-20 17:13:38 +0200

    I upgraded the Tests

--- BudgetTest/BudgetTest.java
+++ BudgetTest/BudgetTest.java
@@ -1,29 +1,30 @@
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import java.io.IOException;
+import java.text.ParseException;
+import org.junit.jupiter.api.Assertions;
 import java.time.LocalDate;
 public class BudgetTest {
-        @Test
-        public void test1_suma() throws IOException {
 
-            Budget budget = new Budget();
-            budget.setSum(3);
-            Assertions.assertEquals(3, budget.getSum());
-        }
+    @Test
+    public void test1() throws IOException, ParseException {
+        String dateString = "2023-12-31";
+        LocalDate date = LocalDate.parse(dateString);
+        Budget budget1 = new Budget(date,"aa","1",24);
+        Budget budget2 = new Budget(date,"aa","2",24);
+        Budget budget4 = new Budget(date,"aa","1",24);
 
-        @Test
-        public void test3() throws IOException {
 
-            Budget budget = new Budget();
-            budget.setName("aaa");
-            Assertions.assertEquals("aaa", budget.getName());
-        }
+        Assertions.assertEquals(2, Budget.getCategories().size());
+    }
 
-        @Test
-        public void test4() throws IOException {
+    @Test
+    public void test3() throws IOException, ParseException {
+        String dateString = "2023-12-31";
+        LocalDate date = LocalDate.parse(dateString);
+        Budget budget1 = new Budget(date,"a","1",24);
+        Budget budget2 = new Budget(date,"a","2",24);
+        Budget budget4 = new Budget(date,"a","3",24);
 
-            Budget budget = new Budget();
-            budget.setCategory("bbb");
-            Assertions.assertEquals("bbb", budget.getCategory());
-        }
+        Assertions.assertEquals(3, Budget.getCategories().size());
+    }
 }
