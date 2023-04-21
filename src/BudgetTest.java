0776285197394738604baa32ad7ea1eac1a81fb7
paula <paulapscx@gmail.com>
2023-04-21 17:16:18 +0200

    perfected the BudgetTest class

--- src/BudgetTest.java
+++ src/BudgetTest.java
@@ -3,16 +3,16 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.text.ParseException;
 import java.time.LocalDate;
+
 public class BudgetTest {
 
     @Test
     public void test1() throws IOException, ParseException {
         String dateString = "2023-12-31";
         LocalDate date = LocalDate.parse(dateString);
-        Budget budget1 = new Budget(date,"aa","1",24);
-        Budget budget2 = new Budget(date,"aa","2",24);
-        Budget budget4 = new Budget(date,"aa","1",24);
-
+        Budget budget1 = new Budget(date, "aa", "1", 24);
+        Budget budget2 = new Budget(date, "aa", "2", 24);
+        Budget budget4 = new Budget(date, "aa", "1", 24);
 
         Assertions.assertEquals(2, Budget.getCategories().size());
     }
@@ -21,12 +21,13 @@ public class BudgetTest {
     public void test3() throws IOException, ParseException {
         String dateString = "2023-12-31";
         LocalDate date = LocalDate.parse(dateString);
-        Budget budget1 = new Budget(date,"a","1",24);
-        Budget budget2 = new Budget(date,"a","2",24);
-        Budget budget4 = new Budget(date,"a","3",24);
+        Budget budget1 = new Budget(date, "a", "1", 24);
+        Budget budget2 = new Budget(date, "a", "2", 24);
+        Budget budget4 = new Budget(date, "a", "3", 24);
 
         Assertions.assertEquals(3, Budget.getCategories().size());
     }
+
     @Test
     public void testNulls1() throws IOException, ParseException {
         String dateString = "2023-12-31";
@@ -48,6 +49,5 @@ public class BudgetTest {
         } catch (NullPointerException e) {
             Assertions.assertEquals("Каиегория не может быть null", e.getMessage());
         }
-
     }
 }
